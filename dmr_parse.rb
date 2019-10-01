c74527a33315679bcb3837f5661609c4ee7d4fcc
Rick Farina (Zero_Chaos) <zerochaos@gentoo.org>
2019-09-30 21:53:14 -0400

    thanks to upstream, here is some working code that doesn't require special patches

--- dmr_parse.rb
+++ dmr_parse.rb
@@ -30,29 +30,37 @@ class Dmrrr
   end
 
   def led(state, ber=0)
-    return unless @@led
-    if state == "ended"
-      BlinkStick.find_all.each { |b| b.off }
-    elsif state == "rx"
-      BlinkStick.find_all.each { |b|
-        b.set_color(0, 0, Color::RGB.new(0,255,0))
-        b.set_color(0, 1, Color::RGB.new(0,255,0))
-      }
-    elsif state == "tx"
-      BlinkStick.find_all.each { |b|
-        b.set_color(0, 0, Color::RGB.new(255,0,0))
-        b.set_color(0, 1, Color::RGB.new(255,0,0))
-      }
-    elsif state == "int"
-      BlinkStick.find_all.each { |b|
-        b.set_color(0, 0, Color::RGB.new(178,0,255))
-        b.set_color(0, 1, Color::RGB.new(178,0,255))
-      }
-    else
-      BlinkStick.find_all.each { |b|
-        b.set_color(0, 0, Color::RGB.new(255,255,255))
-        b.set_color(0, 1, Color::RGB.new(255,255,255))
-      }
+    begin
+      return unless @@led
+      if state == "ended"
+        BlinkStick.find_all.each { |b|
+          b.set_color(0, 0, Color::RGB.new(0,0,0))
+          b.set_color(0, 1, Color::RGB.new(0,0,0))
+        }
+      elsif state == "rx"
+        BlinkStick.find_all.each { |b|
+          b.set_color(0, 0, Color::RGB.new(0,255,0))
+          b.set_color(0, 1, Color::RGB.new(0,255,0))
+        }
+      elsif state == "tx"
+        BlinkStick.find_all.each { |b|
+          b.set_color(0, 0, Color::RGB.new(255,0,0))
+          b.set_color(0, 1, Color::RGB.new(255,0,0))
+        }
+      elsif state == "int"
+        BlinkStick.find_all.each { |b|
+          b.set_color(0, 0, Color::RGB.new(178,0,255))
+          b.set_color(0, 1, Color::RGB.new(178,0,255))
+        }
+      else
+        BlinkStick.find_all.each { |b|
+          b.set_color(0, 0, Color::RGB.new(255,255,255))
+          b.set_color(0, 1, Color::RGB.new(255,255,255))
+        }
+      end
+    rescue => e
+      @@logger.debug(e.inspect)
+      @@logger.debug(e.backtrace)
     end
   end
 
@@ -102,6 +110,7 @@ class Dmrrr
       end
     rescue => e
       @@logger.debug(e.inspect)
+      @@logger.debug(e.backtrace)
       exit 1
     end
   end
@@ -120,6 +129,7 @@ class Dmrrr
       end
     rescue => e
       @@logger.debug(e.inspect)
+      @@logger.debug(e.backtrace)
       exit 1
     ensure
       @logola.close if @logola
@@ -127,48 +137,55 @@ class Dmrrr
   end
 
   def parser_thread
-  begin
-    Thread.new do
-    @loggie = File.open("dmr_parse.raw", "a")
-    @loggie.sync = true
-    #PTY.spawn("#{MD380TOOLS}md380-tool dmesgtail") do |stdout, stdin, pid|
-    PTY.spawn("ssh root@192.168.1.232 'tail -q -n0 -f /var/log/MMDVM-*.log'") do |stdout, stdin, pid|
+    begin
+      Thread.new do
+        @loggie = File.open("dmr_parse.raw", "a")
+        @loggie.sync = true
+        #PTY.spawn("#{MD380TOOLS}md380-tool dmesgtail") do |stdout, stdin, pid|
+        PTY.spawn("ssh root@192.168.1.232 'tail -q -n0 -f /var/log/MMDVM-*.log'") do |stdout, stdin, pid|
           stdin.close
           stdout.each do |line|
-          line.chomp!
-          next if line.empty?
-          next unless line[0..1] == '[* ]' || line[0..1] == 'M:'
-          received_time = Time.now.to_f
-          message = parse_line("#{received_time} #{line.strip}")
-          self.radio_queue << message
+            line.chomp!
+            next if line.empty?
+            next unless line[0..1] == '[* ]' || line[0..1] == 'M:'
+            received_time = Time.now.to_f
+            message = parse_line("#{received_time} #{line.strip}")
+            self.radio_queue << message
+          end
         end
       end
-    end
     rescue
       @@logger.debug(e.inspect)
+      @@logger.debug(e.backtrace)
       exit 1
     ensure
       @loggie.close if @loggie
     end
   end
 
-  def parse_line(line)
 #mmdvm
 #M: 2019-09-06 18:43:57.822 DMR Slot 2, received RF voice header from 47 to TG 1
 #M: 2019-09-06 18:43:58.003 DMR Slot 2, received RF end of voice transmission, 0.0 seconds, BER: 0.0%
 #dmesgtail
 #1507594330 * Call from 3133002 to group 3181 started.
 #1507594351 * Call from 3133002 to group 3181 ended.
-    @loggie.write("#{line}\n")
-    line = line.split(" ")
-    if line[9] == 'voice'
-      return Message.new(line[12],line[15],'started.',line[0].to_f,GROUP)
-    elsif line[9] == 'end'
-      return Message.new(line[14],line[16].gsub(',',''),'ended.',line[0].to_f,GROUP)
-    elsif line[6] == 'group'
-      return Message.new(line[4],line[7],line.last,line[0].to_f,GROUP)
-    elsif line =~ /Call from/
-      return Message.new(line[4],line[6],line.last,line[0].to_f,PRIV)
+  def parse_line(line)
+    begin
+      @loggie.write("#{line}\n")
+      line = line.split(" ")
+      if line[9] == 'voice'
+        return Message.new(line[12],line[15],'started.',line[0].to_f,GROUP)
+      elsif line[9] == 'end'
+        return Message.new(line[14],line[17].gsub(',',''),'ended.',line[0].to_f,GROUP)
+      elsif line[6] == 'group'
+        return Message.new(line[4],line[7],line.last,line[0].to_f,GROUP)
+      elsif line =~ /Call from/
+        return Message.new(line[4],line[6],line.last,line[0].to_f,PRIV)
+      end
+    rescue => e
+      @@logger.debug(e.inspect)
+      @@logger.debug(e.backtrace)
+      exit 1
     end
   end
 
@@ -194,13 +211,13 @@ class Dmrrr
 
   def complete_call(message)
     begin
-    puts "tx completed   : #{message.caller_id} #{message.user} to #{message.call_type} #{message.receiver_id} #{message.tg}, duration: #{message.time_diff(self.current_call.received_time)}s"
-    @twitter_queue.push "#{Time.at(self.current_call.received_time).to_s} - #{message.user} transmitted to #{message.call_type} #{message.tg}, duration: #{message.time_diff(self.current_call.received_time)}s #derbycon" unless message.time_diff(self.current_call.received_time) < 1
-    self.current_call = nil
-    @last_speaker.unshift "#{@currently_speaking} #{Time.at(message.received_time).to_s}"
-    @last_speaker.pop
-    @currently_speaking = ""
-    led("ended")
+      puts "tx completed   : #{message.caller_id} #{message.user} to #{message.call_type} #{message.receiver_id} #{message.tg}, duration: #{message.time_diff(self.current_call.received_time)}s"
+      @twitter_queue.push "#{Time.at(self.current_call.received_time).to_s} - #{message.user} transmitted to #{message.call_type} #{message.tg}, duration: #{message.time_diff(self.current_call.received_time)}s #derbycon" unless message.time_diff(self.current_call.received_time) < 1
+      self.current_call = nil
+      @last_speaker.unshift "#{@currently_speaking} #{Time.at(message.received_time).to_s}"
+      @last_speaker.pop
+      @currently_speaking = ""
+      led("ended")
     rescue => e
       @@logger.debug(e.inspect)
       @@logger.debug(e.backtrace)
@@ -210,11 +227,11 @@ class Dmrrr
 
   def start_new_call(message)
     begin
-    puts "tx started     : #{message.caller_id} #{message.user} to #{message.call_type} #{message.receiver_id} #{message.tg}"
-    self.current_call = message
-    @currently_speaking = "#{message.caller_id} #{message.user}"
-    @most_recent[message.caller_id] = "#{message.user} #{Time.at(message.received_time).to_s}"
-    led("rx")
+      puts "tx started     : #{message.caller_id} #{message.user} to #{message.call_type} #{message.receiver_id} #{message.tg}"
+      self.current_call = message
+      @currently_speaking = "#{message.caller_id} #{message.user}"
+      @most_recent[message.caller_id] = "#{message.user} #{Time.at(message.received_time).to_s}"
+      led("rx")
     rescue => e
       @@logger.debug(e.inspect)
       @@logger.debug(e.backtrace)
@@ -224,12 +241,12 @@ class Dmrrr
 
   def call_inturrupted(message)
     begin
-    puts "tx interrupted : #{message.caller_id} #{message.user} interrupted #{self.current_call.caller_id} #{self.current_call.user} like a real asshole, duration: #{message.time_diff(self.current_call.received_time)}s"
-    @twitter_queue.push "#{Time.at(self.current_call.received_time).to_s} - #{message.user} interrupted #{self.current_call.user} like a jerk, duration: #{message.time_diff(self.current_call.received_time)}s #derbycon" unless message.time_diff(self.current_call.received_time) < 1
-    self.current_call = message
-    @currently_speaking << ", #{message.caller_id} #{message.user}"
-    @most_recent[message.caller_id] = "#{message.user} #{Time.at(message.received_time).to_s}"
-    led("int")
+      puts "tx interrupted : #{message.caller_id} #{message.user} interrupted #{self.current_call.caller_id} #{self.current_call.user} like a real asshole, duration: #{message.time_diff(self.current_call.received_time)}s"
+      @twitter_queue.push "#{Time.at(self.current_call.received_time).to_s} - #{message.user} interrupted #{self.current_call.user} like a jerk, duration: #{message.time_diff(self.current_call.received_time)}s #derbycon" unless message.time_diff(self.current_call.received_time) < 1
+      self.current_call = message
+      @currently_speaking << ", #{message.caller_id} #{message.user}"
+      @most_recent[message.caller_id] = "#{message.user} #{Time.at(message.received_time).to_s}"
+      led("int")
     rescue => e
       @@logger.debug(e.inspect)
       @@logger.debug(e.backtrace)
@@ -239,41 +256,43 @@ class Dmrrr
 
   def same_id_message(message)
     begin
-    if self.current_call.state == STARTED
-      if message.state == ENDED
-        complete_call(message)
+      if self.current_call.state == STARTED
+        if message.state == ENDED
+          complete_call(message)
+        end
+      else
+        start_new_call(message)
       end
-    else
-      start_new_call(message)
-    end
     rescue => e
       @@logger.debug(e.inspect)
+      @@logger.debug(e.backtrace)
       exit 1
     end
   end
 
   def different_id_message(message)
     begin
-    if self.current_call.state == ENDED
-      start_new_call(message)
-    else
-      call_inturrupted(message)
-    end
+      if self.current_call.state == ENDED
+        start_new_call(message)
+      else
+        call_inturrupted(message)
+      end
     rescue => e
       @@logger.debug(e.inspect)
+      @@logger.debug(e.backtrace)
       exit 1
     end
   end
 
   def process_call(message)
     begin
-    if self.current_call.nil?
-     start_new_call(message)
-    elsif message.caller_id == self.current_call.caller_id
-      same_id_message(message)
-    else
-      different_id_message(message)
-    end
+      if self.current_call.nil?
+        start_new_call(message)
+      elsif message.caller_id == self.current_call.caller_id
+        same_id_message(message)
+      else
+        different_id_message(message)
+      end
     rescue => e
       @@logger.debug(e.inspect)
       @@logger.debug(e.backtrace)
@@ -294,96 +313,147 @@ class Message
   @@db =  nil
   @@db_last = 0
 
+  def initialize(c_id,r_id,message_type,time,type)
+    begin
+      self.class.users
+      self.caller_id = c_id
+      self.receiver_id = r_id
+      self.state = message_type
+      self.received_time = time
+      self.call_type = type
+    rescue => e
+      puts e.inspect
+      puts e.backtrace
+      exit 1
+    end
+  end
+
   def user
-    user = @@db[self.caller_id]
-    if user.nil? || user.empty?
-      if (Time.now.to_i - @@db_last) > 60
-        puts @@db_last
-        Message.users(true)
-      end
-    end
-
-    if user.nil?
-      user = "UNALLOCATED"
-    elsif user.empty?
+    begin
       user = @@db[self.caller_id]
-      if user.empty? || user.nil?
-        user = "jerk who did not check their radio out properly"
+      if user.nil? || user.empty?
+        if (Time.now.to_i - @@db_last) > 60
+          puts @@db_last
+          Message.users(true)
+        end
       end
-    end
 
-    return user
+      if user.nil?
+        user = "UNALLOCATED"
+      elsif user.empty?
+        user = @@db[self.caller_id]
+        if user.empty? || user.nil?
+          user = "jerk who did not check their radio out properly"
+        end
+      end
+
+      return user
+    rescue => e
+      puts e.inspect
+      puts e.backtrace
+      exit 1
+    end
   end
 
   def tg
-    if self.receiver_id == "1"
-      tg = "Derbycon"
-    else
-      tg = "the wrong talkgroup, so no one heard it"
-    end
+    begin
+      if self.receiver_id == '1'
+        tg = 'Derbycon'
+      elsif self.receiver_id == '98638'
+        tg = 'WVNet'
+      else
+        tg = 'the wrong talkgroup, so no one heard it'
+      end
 
-    return tg
+      return tg
+    rescue => e
+      puts e.inspect
+      puts e.backtrace
+      exit 1
+    end
   end
 
   def self.users(override=false)
-    if (Time.now.to_i - @@db_last) > 600 || override
-      self.update_db
-      #csv_file = CSV.parse("Radio ID,Callsign,Name,City,State,Country,Remarks\n#{File.read("#{MD380TOOLS}/db/stripped.csv").gsub('"', "'")}", {:headers => true, :header_converters => :symbol})
-      #csv_file = CSV.parse("Name,Callsign,Radio ID,City\n#{File.read("#{MD380TOOLS}/db/stripped.csv").gsub('"', "'")}", {:headers => true, :header_converters => :symbol})
-      hashie = Hash.new
-      #csv_file.map(&:to_hash).each{|row| hashie[row.delete(:radio_id)] = row }
-      x = File.read '/tmp/custom.csv'
-      hashie = Hash[x.split("\n").map{|y| y.split(',')[0,2] }] 
-      #hashie["9046"] = "Medic"
-      @@db = hashie
-      @@db_last = Time.now.to_i
-      puts "#{:user_list}"
-      puts "#{@@db}"
+    begin
+      if (Time.now.to_i - @@db_last) > 600 || override
+        #self.update_db
+        #csv_file = CSV.parse("Radio ID,Callsign,Name,City,State,Country,Remarks\n#{File.read("#{MD380TOOLS}/db/stripped.csv").gsub('"', "'")}", {:headers => true, :header_converters => :symbol})
+        #csv_file = CSV.parse("Name,Callsign,Radio ID,City\n#{File.read("#{MD380TOOLS}/db/stripped.csv").gsub('"', "'")}", {:headers => true, :header_converters => :symbol})
+        hashie = Hash.new
+        #csv_file.map(&:to_hash).each{|row| hashie[row.delete(:radio_id)] = row }
+        #x = File.read '/tmp/custom.csv'
+        #hashie = Hash[x.split("\n").map{|y| y.split(',')[0,2] }] 
+        x = File.read 'DMRIds.dat'
+        hashie = Hash[x.split("\n").map{|y| y.split("\t")[0,2] }].invert
+        @@db = hashie
+        @@db_last = Time.now.to_i
+        puts "#{:user_list}"
+        puts "#{@@db}"
+      end
+    rescue => e
+      puts e.inspect
+      puts e.backtrace
+      exit 1
     end
   end
 
-  def initialize(c_id,r_id,message_type,time,type)
-    self.class.users
-    self.caller_id = c_id
-    self.receiver_id = r_id
-    self.state = message_type
-    self.received_time = time
-    self.call_type = type
-  end
-
-
   def time_diff(current_time)
-    return (self.received_time - current_time).round(2)
+    begin
+      return (self.received_time - current_time).round(2)
+    rescue => e
+      puts e.inspect
+      puts e.backtrace
+      exit 1
+    end
   end
 
   def to_s
-  [
-  @@id_lookup[self.caller_id],
-  "-->",
-  self.receiver_id
-  ].join(' ')
+    begin
+      [
+        @@id_lookup[self.caller_id],
+        "-->",
+        self.receiver_id
+      ].join(' ')
+    rescue => e
+      puts e.inspect
+      puts e.backtrace
+      exit 1
+    end
   end
  
   def self.update_derby_db
-    puts "updating db"
-    #`curl 'https://docs.google.com/spreadsheets/d/1ph5Wh0Wztljtdd5PLKGipwEuYE3LIHT-1wSOs-e6iV4/export?format=csv&id=1ph5Wh0Wztljtdd5PLKGipwEuYE3LIHT-1wSOs-e6iV4&gid=0' > /tmp/custom.csv`
-    #`curl 'https://docs.google.com/spreadsheets/d/1ph5Wh0Wztljtdd5PLKGipwEuYE3LIHT-1wSOs-e6iV4/export?format=csv&id=1ph5Wh0Wztljtdd5PLKGipwEuYE3LIHT-1wSOs-e6iV4&gid=1919271561' > /tmp/custom1.csv`
-    `curl 'https://docs.google.com/spreadsheets/d/1bAIPm1okjKVg3mhWBd2lGjKGp9sMpVsN_Tc0yDzda-c/export?format=csv' > /tmp/custom1.csv`
-    `sed -i '1d' /tmp/custom1.csv`
-    `dos2unix /tmp/custom1.csv > /dev/null 2>&1`
-    `awk -F',' '{print $3","$2}' /tmp/custom1.csv > /tmp/custom2.csv`
-    `[ -n "$(cat /tmp/custom2.csv)" ] && mv /tmp/custom2.csv /tmp/custom.csv`
-    puts "#{:db_done}"
+    begin
+      puts "updating db"
+      #`curl 'https://docs.google.com/spreadsheets/d/1ph5Wh0Wztljtdd5PLKGipwEuYE3LIHT-1wSOs-e6iV4/export?format=csv&id=1ph5Wh0Wztljtdd5PLKGipwEuYE3LIHT-1wSOs-e6iV4&gid=0' > /tmp/custom.csv`
+      #`curl 'https://docs.google.com/spreadsheets/d/1ph5Wh0Wztljtdd5PLKGipwEuYE3LIHT-1wSOs-e6iV4/export?format=csv&id=1ph5Wh0Wztljtdd5PLKGipwEuYE3LIHT-1wSOs-e6iV4&gid=1919271561' > /tmp/custom1.csv`
+      `curl 'https://docs.google.com/spreadsheets/d/1bAIPm1okjKVg3mhWBd2lGjKGp9sMpVsN_Tc0yDzda-c/export?format=csv' > /tmp/custom1.csv`
+      `sed -i '1d' /tmp/custom1.csv`
+      `dos2unix /tmp/custom1.csv > /dev/null 2>&1`
+      `awk -F',' '{print $3","$2}' /tmp/custom1.csv > /tmp/custom2.csv`
+      `[ -n "$(cat /tmp/custom2.csv)" ] && mv /tmp/custom2.csv /tmp/custom.csv`
+      puts "#{:db_done}"
+    rescue => e
+      puts e.inspect
+      puts e.backtrace
+      exit 1
+    end
   end
 
   def self.update_db
-    puts "#{:updating_db}"
-    #`cd #{MD380TOOLS}; make updatedb`
-    puts "#{:intentionally_nerfed}"
-    update_derby_db
-    puts "#{:updated}"
+    begin
+      puts "#{:updating_db}"
+      #`cd #{MD380TOOLS}; make updatedb`
+      puts "#{:intentionally_nerfed}"
+      update_derby_db
+      puts "#{:updated}"
+    rescue => e
+      puts e.inspect
+      puts e.backtrace
+      exit 1
+    end
   end
 
+  #this is just to show it all worked
   users
 
 end
